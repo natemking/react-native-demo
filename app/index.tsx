1c4f0cfcac543f2cfaf379da4989fb4a17b83653
Nate M King <natemking@gmail.com>
2025-11-05 14:14:25 -0500

    feat: addded img and styles. Also updated tsconfig.json to suppot paths

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const HomePage = () => {
+	const { container, img, title } = styles;
 	return (
-		<View>
-			<Text>HomePage</Text>
+		<View style={container}>
+            <Image source={require('img/logo_light.png')} style={img} />
+
+			<Text style={title}>The Number 1</Text>
+			<Text style={{ marginTop: 10, marginBottom: 30 }}>
+				Reading List App
+			</Text>
 		</View>
 	);
 };
 
 export default HomePage;
-const styles = StyleSheet.create({});
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontWeight: '700',
+		fontSize: 18,
+	},
+    img: {
+        marginVertical: 20
+    },
+});
