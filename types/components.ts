b1ecedc09a6ec26813dd3fe695801a34a9f10d65
Nate M King <natemking@gmail.com>
2025-11-05 16:20:42 -0500

    feat: created types dir, index, & compoents file. added type for ThemedView componet. updated the tsconfig paths and added type to ThemedView

---
+++ types/components.ts
@@ -0,0 +1,5 @@
+import { StyleProp, ViewStyle } from 'react-native'
+
+export type ThemedViewProps = {
+    style?: StyleProp<ViewStyle>
+}
--- ui/components/ThemedView.tsx
+++ ui/components/ThemedView.tsx
@@ -1,13 +1,14 @@
 import { View, Text, useColorScheme } from 'react-native';
 import { colors } from 'styles/colors';
+import { ThemedViewProps } from 'types';
 
-const ThemedView = () => {
+const ThemedView = ({ style }: ThemedViewProps) => {
 	const colorScheme = useColorScheme();
 	const theme = colors[colorScheme ?? 'light'];
 	const { background } = theme;
 
 	return (
-		<View style={[{ backgroundColor: background }]}>
+		<View style={[{ backgroundColor: background }, style]}>
 			<Text>ThemedView</Text>
 		</View>
 	);
