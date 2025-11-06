c7544e06d27265bafa77790c6a1c02356abfb90d
Nate M King <natemking@gmail.com>
2025-11-06 14:45:49 -0500

    feat: added ThemedBtn to register page

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,36 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { Link } from 'expo-router';
 import { Spacer } from 'components/Spacer';
 import { ThemedView } from 'components/ThemedView';
 import { ThemedText } from 'components/ThemedText';
 import { loginUrl } from 'lib/constants';
+import { ThemedBtn } from 'components/ThemedBtn';
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-    },
-    title: {
-        textAlign: 'center',
-        fontSize: 18,
-        marginBottom: 30
-    }
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+        alignItems: 'center'
+	},
+	title: {
+		textAlign: 'center',
+		fontSize: 18,
+		marginBottom: 30,
+	},
 });
 
 export default function RegisterPage(): React.JSX.Element {
-    const { container, title } = styles;
-    return (
-        <ThemedView style={container}>
-            <Spacer />
-            <ThemedText isTitle style={title}>
-                Register for an Account
-            </ThemedText>
-            <Spacer height={100} />
-            <Link href={loginUrl}>
-                <ThemedText style={{ textAlign: 'center' }}>
-                    Login Instead
-                </ThemedText>
-            </Link>
-        </ThemedView>
-    );
-};
+	const { container, title } = styles;
+
+	const handleSubmit = () => {
+		console.log('register form submitted');
+	};
+
+	return (
+		<ThemedView style={container}>
+			<Spacer />
+			<ThemedText isTitle style={title}>
+				Register for an Account
+			</ThemedText>
+			<ThemedBtn onPress={handleSubmit}>
+				<Text style={{ color: 'hsl(0 0% 95%)' }}>Register</Text>
+			</ThemedBtn>
+			<Spacer height={100} />
+			<Link href={loginUrl}>
+				<ThemedText style={{ textAlign: 'center' }}>
+					Login Instead
+				</ThemedText>
+			</Link>
+		</ThemedView>
+	);
+}
