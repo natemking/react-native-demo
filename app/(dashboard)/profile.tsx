e08a029bc95df38d115a323b3af97938093d859d
Nate M King <natemking@gmail.com>
2025-11-07 15:41:13 -0500

    feat: added logout btn to profile

--- app/(dashboard)/profile.tsx
+++ app/(dashboard)/profile.tsx
@@ -1,10 +1,16 @@
+import { Spacer } from 'components/Spacer';
 import { ThemedView } from 'components/ThemedView';
 import { ThemedText } from 'components/ThemedText';
-import { Spacer } from 'components/Spacer';
-import { globalStyles } from 'styles';
+import { useUser } from 'hooks';
+import { colors, globalStyles } from 'styles';
+import { ThemedBtn } from 'components/ThemedBtn';
+import { Text } from 'react-native';
 
 export default function ProfilePage() {
 	const { container, heading } = globalStyles;
+
+	const { logout } = useUser();
+
 	return (
 		<ThemedView style={container}>
 			<ThemedText isTitle style={heading}>
@@ -14,6 +20,9 @@ export default function ProfilePage() {
 
 			<ThemedText>Time to start reading some books...</ThemedText>
 			<Spacer />
+			<ThemedBtn onPress={logout}>
+				<Text style={{ color: colors.primaryForeground }}>Logout</Text>
+			</ThemedBtn>
 		</ThemedView>
 	);
 }
