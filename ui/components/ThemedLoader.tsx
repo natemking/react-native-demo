3191c51642ed69d29c1f0f609c3a2d339b5c5227
Nate M King <natemking@gmail.com>
2025-11-10 09:44:52 -0500

    feat: created ThemedLoader

---
+++ ui/components/ThemedLoader.tsx
@@ -0,0 +1,17 @@
+import { ActivityIndicator } from 'react-native';
+import { ThemedView } from 'components/ThemedView';
+import { useTheme } from 'hooks';
+import { globalStyles } from 'styles';
+
+export default function ThemedLoader() {
+	const theme = useTheme();
+
+	const { text } = theme;
+	const { container } = globalStyles;
+
+	return (
+		<ThemedView style={container}>
+			<ActivityIndicator size='large' color={text} />
+		</ThemedView>
+	);
+}
